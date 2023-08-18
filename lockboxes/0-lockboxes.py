d216390c4acba057644b4422e47add2a60b0ee7f
Salma5806 <5806@holbertonstudents.com>
2023-08-18 19:07:12 +0100

    boxes

---
+++ lockboxes/0-lockboxes.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+""" solve lockboxes problem  """
+
+
+def canUnlockAll(boxes):
+    """determines if all boxes can be unlocked"""
+    keys = [0]
+    for key in keys:
+        for box_key in boxes[key]:
+            if box_key < len(boxes) and box_key not in keys:
+                keys.append(box_key)
+    if len(keys) == len(boxes):
+        return True
+    else:
+        return False   
