a2a2c096d3cf03cca77c790d26caacc4e00590d8
Salma5806 <5806@holbertonstudents.com>
2023-09-08 22:51:48 +0100

    min op

--- minimum_operations/0-minoperations.py
+++ minimum_operations/0-minoperations.py
@@ -6,14 +6,15 @@ def minOperations(n):
     """min opearation"""
     if n <= 1:
         return 0
-    
-    dp = [0] * (n + 1)
-    
-    for i in range(2, n + 1):
-        dp[i] = i
-        
-        for j in range(2, i // 2 + 1):
-            if i % j == 0:
-                dp[i] = min(dp[i], dp[j] + i // j)
-    
-    return dp[n]
+
+    operations = 0
+    divisor = 2
+
+    while n > 1:
+        while n % divisor == 0:
+            n //= divisor
+            operations += divisor
+
+        divisor += 1
+
+    return operations
