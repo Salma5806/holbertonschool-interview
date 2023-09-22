c0cb3a9bb976d04bfa1921a916b3ec4ac6c7239b
Salma5806 <5806@holbertonstudents.com>
2023-09-22 23:10:31 +0100

    sandples

--- sandpiles/sandpiles.h
+++ sandpiles/sandpiles.h
@@ -9,4 +9,4 @@ void sandpiles_sum(int grid1[3][3], int grid2[3][3]);
 int grid1_unstable(int grid1[3][3], int flags[3][3]);
 void topple(int grid1[3][3], int i, int j);
 
-#endif
+#endif  /* SANDPILES_H */
