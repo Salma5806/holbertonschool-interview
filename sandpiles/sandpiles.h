357804f4b9097a42a9f835701767c33ec086aadc
Salma5806 <5806@holbertonstudents.com>
2023-09-22 23:08:00 +0100

    sandples

--- sandpiles/sandpiles.h
+++ sandpiles/sandpiles.h
@@ -1,13 +1,12 @@
 #ifndef SANDPILES_H
+#ifndef SANDPILES_H
 #define SANDPILES_H
 
 #include <stdlib.h>
-#include <stdio.h>
-
-void sandpiles_sum(int grid1[3][3], int grid2[3][3]);
+#include <stdio.h>/
 static void print_grid(int grid[3][3]);
-void sum_grids(int grid1[3][3], int grid2[3][3]);
-void change_grids(int grid1[3][3]);
-int check_grids(int grid1[3][3]);
+void sandpiles_sum(int grid1[3][3], int grid2[3][3]);
+int grid1_unstable(int grid1[3][3], int flags[3][3]);
+void topple(int grid1[3][3], int i, int j);
 
 #endif
