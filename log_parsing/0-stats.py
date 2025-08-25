3e9b9114da9a52670a0fe1e55fa1210afb5a4309
Salma5806 <113902287+Salma5806@users.noreply.github.com>
2025-08-25 20:32:46 +0100

    Update 0-stats.py

--- log_parsing/0-stats.py
+++ log_parsing/0-stats.py
@@ -1,43 +1,44 @@
 #!/usr/bin/python3
-"""Log parsing"""
+"""
+This module tracks input statistics for file size and HTTP status codes.
+"""
 
-from sys import stdin
+import sys
 
-if __name__ == "__main__":
-    total_size = 0
-    status_codes = {}
-    list_status_codes = [
-        "200", "301", "400", "401", "403", "404", "405", "500"]
-    for status in list_status_codes:
-        status_codes[status] = 0
-    count = 0
-    try:
-        for line in stdin:
-            try:
-                args = line.split(" ")
-                if len(args) != 9:
-                    pass
-                if args[-2] in list_status_codes:
-                    status_codes[args[-2]] += 1
-                if args[-1][-1] == '\n':
-                    args[-1][:-1]
-                total_size += int(args[-1])
-            except:
-                pass
-            count += 1
-            if count % 10 == 0:
-                print("File size: {}".format(total_size))
-                for status in sorted(status_codes.keys()):
-                    if status_codes[status] != 0:
-                        print("{}: {}".format(
-                            status, status_codes[status]))
-        print("File size: {}".format(total_size))
-        for status in sorted(status_codes.keys()):
-            if status_codes[status] != 0:
-                print("{}: {}".format(status, status_codes[status]))
-    except KeyboardInterrupt as err:
-        print("File size: {}".format(total_size))
-        for status in sorted(status_codes.keys()):
-            if status_codes[status] != 0:
-                print("{}: {}".format(status, status_codes[status]))
-        raise
+status_counts = {
+    '200': 0,
+    '301': 0,
+    '400': 0,
+    '401': 0,
+    '403': 0,
+    '404': 0,
+    '405': 0,
+    '500': 0
+}
+
+file_sizes = [0]
+
+def display_statistics():
+    total_size = sum(file_sizes)
+    print('File size: {}'.format(total_size))
+    for code, quantity in sorted(status_counts.items()):
+        if quantity > 0:
+            print('{}: {}'.format(code, quantity))
+
+try:
+    for line_number, entry in enumerate(sys.stdin, start=1):
+        data = entry.strip().split()
+        try:
+            status = data[-2]
+            size = data[-1]
+            if status in status_counts:
+                status_counts[status] += 1
+            file_sizes.append(int(size))
+        except (IndexError, ValueError):
+            continue
+        if line_number % 10 == 0:
+            display_statistics()
+    display_statistics()
+except KeyboardInterrupt:
+    display_statistics()
+    raise
