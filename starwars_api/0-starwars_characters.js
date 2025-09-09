6d8166ee1c11438d1cd9e183e725db057c667e36
Salma5806 <113902287+Salma5806@users.noreply.github.com>
2025-09-09 22:01:34 +0100

    Update 0-starwars_characters.js

--- starwars_api/0-starwars_characters.js
+++ starwars_api/0-starwars_characters.js
@@ -1,54 +1,38 @@
 #!/usr/bin/node
+// 0-starwars_characters.js
+// Prints all characters of a Star Wars movie
 
-const https = require('https');
+const request = require('request');
+
+if (process.argv.length !== 3) {
+  console.log('Usage: ./0-starwars_characters.js <movie_id>');
+  process.exit(1);
+}
 
 const movieId = process.argv[2];
-const apiUrl = `https://swapi-api.hbtn.io/api/films/${movieId}/`;
+const url = `https://swapi.dev/api/films/${movieId}/`;
 
-https.get(apiUrl, (response) => {
-  let data = '';
+request(url, { json: true }, (err, res, body) => {
+  if (err) {
+    console.error(err);
+    return;
+  }
 
-  response.on('data', (chunk) => {
-    data += chunk;
+  if (!body.characters) {
+    console.error('No characters found.');
+    return;
+  }
+
+  const characters = body.characters;
+
+  // Fetch each character name in order
+  characters.forEach((charUrl) => {
+    request(charUrl, { json: true }, (err, res, charBody) => {
+      if (err) {
+        console.error(err);
+        return;
+      }
+      console.log(charBody.name);
+    });
   });
-
-  response.on('end', () => {
-    if (response.statusCode !== 200) {
-      console.error(`Error: Status code ${response.statusCode}`);
-      return;
-    }
-
-    const filmData = JSON.parse(data);
-    const charactersUrls = filmData.characters;
-
-    const fetchCharacterNames = (urls, index = 0) => {
-      if (index >= urls.length) return;
-
-      https.get(urls[index], (charResponse) => {
-        let charData = '';
-
-        charResponse.on('data', (chunk) => {
-          charData += chunk;
-        });
-
-        charResponse.on('end', () => {
-          if (charResponse.statusCode !== 200) {
-            console.error(`Error: Status code ${charResponse.statusCode}`);
-            return;
-          }
-
-          const characterData = JSON.parse(charData);
-          console.log(characterData.name);
-
-          fetchCharacterNames(urls, index + 1);
-        });
-      }).on('error', (charError) => {
-        console.error(charError);
-      });
-    };
-
-    fetchCharacterNames(charactersUrls);
-  });
-}).on('error', (error) => {
-  console.error(error);
 });
