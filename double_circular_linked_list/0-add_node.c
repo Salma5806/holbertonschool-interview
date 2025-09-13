2d15dbd4de427e12dcd3b43ed4a87e27e2ca0f7e
Salma5806 <113902287+Salma5806@users.noreply.github.com>
2025-09-13 20:20:34 +0100

    Update 0-add_node.c

--- double_circular_linked_list/0-add_node.c
+++ double_circular_linked_list/0-add_node.c
@@ -1,66 +1,86 @@
-#include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include "list.h"
 
 /**
- * add_node_end - Adds a new node to the end of a double circular list
+ * add_node_end - Add a new node at the end of a double circular linked list
  * @list: Pointer to the head of the list
- * @str: String to copy into the new node
- *
+ * @str: String to store in the new node
  * Return: Address of the new node, or NULL on failure
  */
 List *add_node_end(List **list, char *str)
 {
-    List *new, *tail;
+	List *new, *last;
 
-    if (!list)
-        return (NULL);
+	if (!list)
+		return (NULL);
 
-    new = malloc(sizeof(List));
-    if (!new)
-        return (NULL);
+	new = malloc(sizeof(List));
+	if (!new)
+		return (NULL);
 
-    new->str = strdup(str);
-    if (!new->str)
-    {
-        free(new);
-        return (NULL);
-    }
+	new->str = strdup(str);
+	if (!new->str)
+	{
+		free(new);
+		return (NULL);
+	}
 
-    if (*list == NULL)
-    {
-        new->next = new;
-        new->prev = new;
-        *list = new;
-        return (new);
-    }
+	if (*list == NULL)
+	{
+		new->next = new;
+		new->prev = new;
+		*list = new;
+		return (new);
+	}
 
-    tail = (*list)->prev;
+	last = (*list)->prev;
+	last->next = new;
+	new->prev = last;
+	new->next = *list;
+	(*list)->prev = new;
 
-    tail->next = new;
-    new->prev = tail;
-    new->next = *list;
-    (*list)->prev = new;
-
-    return (new);
+	return (new);
 }
 
 /**
- * add_node_begin - Adds a new node to the beginning of a double circular list
+ * add_node_begin - Add a new node at the beginning of a double circular linked list
  * @list: Pointer to the head of the list
- * @str: String to copy into the new node
- *
+ * @str: String to store in the new node
  * Return: Address of the new node, or NULL on failure
  */
 List *add_node_begin(List **list, char *str)
 {
-    List *new;
+	List *new, *last;
 
-    new = add_node_end(list, str);
-    if (!new)
-        return (NULL);
+	if (!list)
+		return (NULL);
 
-    *list = new;
-    return (new);
+	new = malloc(sizeof(List));
+	if (!new)
+		return (NULL);
+
+	new->str = strdup(str);
+	if (!new->str)
+	{
+		free(new);
+		return (NULL);
+	}
+
+	if (*list == NULL)
+	{
+		new->next = new;
+		new->prev = new;
+		*list = new;
+		return (new);
+	}
+
+	last = (*list)->prev;
+	new->next = *list;
+	new->prev = last;
+	last->next = new;
+	(*list)->prev = new;
+	*list = new;
+
+	return (new);
 }
